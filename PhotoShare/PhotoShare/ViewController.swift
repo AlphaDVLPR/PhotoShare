7d6bbf27cbce0fa124367116be2910c9cf34061c
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-15 14:27:07 -0600

    Initial Project

---
+++ PhotoShare/PhotoShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoShare
+//
+//  Created by AlphaDVLPR on 8/15/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
